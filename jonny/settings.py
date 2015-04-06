80921c4ed7e00658adbbdc468ed3cdff23247aeb
Zeljko Nesic <popara@gmail.com>
2015-04-06 17:19:34 +0200

    Wsgi

--- jonny/settings.py
+++ jonny/settings.py
@@ -28,8 +28,8 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -76,8 +76,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
