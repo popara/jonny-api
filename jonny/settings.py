b0c3ed5e4c4ba67e3a81041cd61a4fa8e6985629
Zeljko Nesic <popara@gmail.com>
2015-04-20 16:56:51 +0200

    Jebeni kros headeri

--- jonny/settings.py
+++ jonny/settings.py
@@ -1,38 +1,14 @@
-"""
-Django settings for jonny project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'nu04u00k0wm0^qsswa0paf4kyn1xofq6h@vd=l#d#(5l=$iys('
 GOOGLE_PLACES_API_KEY = os.environ.get('GOOGLE_PLACES_API_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
-
-# TEMPLATE_LOADERS = (
-#     ('pyjade.ext.django.Loader',(
-#         'django.template.loaders.filesystem.Loader',
-#         'django.template.loaders.app_directories.Loader',
-#     )),
-# )
 
 ALLOWED_HOSTS = ['*']
 
@@ -50,12 +26,15 @@ INSTALLED_APPS = (
     'plan',
     'storages',
     'rest_framework',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -66,6 +45,27 @@ REST_FRAMEWORK = {
 
 }
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'herokuapp.com',
+    'localhost:8000',
+    'localhost:3333',
+    'jonnyibiza.com',
+    '127.0.0.1:8000',
+)
+CORS_URL_REGEX = r'^/api/.*$'
+CORS_REPLACE_HTTPS_REFERER = True
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken',
+    'cache-control'
+)
+
 ROOT_URLCONF = 'jonny.urls'
 WSGI_APPLICATION = 'jonny.wsgi.application'
 
@@ -74,17 +74,13 @@ DATABASES = {
     'default': dj_database_url.config(default="postgres://jonny:jonny@localhost:5432/jonnyinc")
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 STATIC_ROOT = 'staticfiles'
