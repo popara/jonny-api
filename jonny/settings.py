029677a01b1dc0dbfb6e8072dfe9731320ca0819
Zeljko Nesic <popara@gmail.com>
2015-05-21 13:54:58 +0200

    Message2

--- jonny/settings.py
+++ jonny/settings.py
@@ -55,6 +55,7 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:3000',
     'jonnyibiza.com',
     '127.0.0.1:8000',
+    'development.jonnyinc.divshot.io',
     '*.jonnyinc.divshot.io',
     'jonnyinc.divshot.io',
     'burning-torch-1428.firebaseapp.com',
